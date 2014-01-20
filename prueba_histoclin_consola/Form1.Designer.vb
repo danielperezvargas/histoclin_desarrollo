9e84388258b1e7afedaa2ab4450df6c35eb8f9f3
Daniel Perez Vargas <danielperezvargas@hotmail.com>
2014-01-19 21:49:34 -0600

    cambios varios

--- prueba_histoclin_consola/Form1.Designer.vb
+++ prueba_histoclin_consola/Form1.Designer.vb
@@ -28,7 +28,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(959, 634)
+        Me.ClientSize = New System.Drawing.Size(1038, 402)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
