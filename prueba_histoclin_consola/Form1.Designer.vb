127b6d8b99f31aefb8dc4b9156403fd57f530166
Daniel Perez Vargas <danielperezvargas@hotmail.com>
2014-01-20 17:51:43 -0600

    gfdssfsdfsf

--- prueba_histoclin_consola/Form1.Designer.vb
+++ prueba_histoclin_consola/Form1.Designer.vb
@@ -22,10 +22,18 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ReportDataSource1 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.histoclinDataSet = New prueba_histoclin_consola.histoclinDataSet()
+        Me.bt_canc_dosisunitariaBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.bt_canc_dosisunitariaTableAdapter = New prueba_histoclin_consola.histoclinDataSetTableAdapters.bt_canc_dosisunitariaTableAdapter()
         Me.GroupBox1.SuspendLayout()
+        CType(Me.histoclinDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.bt_canc_dosisunitariaBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'GroupBox1
@@ -34,10 +42,20 @@ Partial Class Form1
         Me.GroupBox1.Controls.Add(Me.Button1)
         Me.GroupBox1.Location = New System.Drawing.Point(36, 24)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(416, 182)
+        Me.GroupBox1.Size = New System.Drawing.Size(175, 182)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Arteaga"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(24, 102)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(85, 21)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "Hinojosa"
+        Me.CheckBox1.UseVisualStyleBackColor = True
         '
         'Button1
         '
@@ -48,30 +66,52 @@ Partial Class Form1
         Me.Button1.Text = "Edgar"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'CheckBox1
+        'ReportViewer1
         '
-        Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(24, 102)
-        Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(100, 21)
-        Me.CheckBox1.TabIndex = 1
-        Me.CheckBox1.Text = "Hinojosa"
-        Me.CheckBox1.UseVisualStyleBackColor = True
+        ReportDataSource1.Name = "DataSet1"
+        ReportDataSource1.Value = Me.bt_canc_dosisunitariaBindingSource
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource1)
+        Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "prueba_histoclin_consola.Report1.rdlc"
+        Me.ReportViewer1.Location = New System.Drawing.Point(234, 24)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.Size = New System.Drawing.Size(946, 632)
+        Me.ReportViewer1.TabIndex = 1
+        '
+        'histoclinDataSet
+        '
+        Me.histoclinDataSet.DataSetName = "histoclinDataSet"
+        Me.histoclinDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'bt_canc_dosisunitariaBindingSource
+        '
+        Me.bt_canc_dosisunitariaBindingSource.DataMember = "bt_canc_dosisunitaria"
+        Me.bt_canc_dosisunitariaBindingSource.DataSource = Me.histoclinDataSet
+        '
+        'bt_canc_dosisunitariaTableAdapter
+        '
+        Me.bt_canc_dosisunitariaTableAdapter.ClearBeforeFill = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1038, 402)
+        Me.ClientSize = New System.Drawing.Size(1339, 695)
+        Me.Controls.Add(Me.ReportViewer1)
         Me.Controls.Add(Me.GroupBox1)
         Me.Name = "Form1"
         Me.Text = "Descargando Edgar..."
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
+        CType(Me.histoclinDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.bt_canc_dosisunitariaBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
     Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents bt_canc_dosisunitariaBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents histoclinDataSet As prueba_histoclin_consola.histoclinDataSet
+    Friend WithEvents bt_canc_dosisunitariaTableAdapter As prueba_histoclin_consola.histoclinDataSetTableAdapters.bt_canc_dosisunitariaTableAdapter
 End Class
--- prueba_histoclin_consola/Form1.vb
+++ prueba_histoclin_consola/Form1.vb
@@ -1,17 +1,16 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        
-        'TODO: esta línea de código carga datos en la tabla 'bt_equipos.bt_equipos' Puede moverla o quitarla según sea necesario.
+        'TODO: esta línea de código carga datos en la tabla 'histoclinDataSet.bt_canc_dosisunitaria' Puede moverla o quitarla según sea necesario.
+        Me.bt_canc_dosisunitariaTableAdapter.Fill(Me.histoclinDataSet.bt_canc_dosisunitaria)
 
-        Me.ReportViewer2.RefreshReport()
-
+        Me.ReportViewer1.RefreshReport()
 
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-        Me.ReportViewer2.ShowExportButton = False
+
 
     End Sub
 
